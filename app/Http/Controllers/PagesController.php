baf0885289afe803d3d366f63d2b0dd7f2769775
anai1203 <anai.flor.angelica@gmail.com>
2022-12-14 16:24:12 -0500

    Actualizado hasta el 14/12/22 a las 04:24 horas

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -25,4 +25,67 @@ class PagesController extends Controller
         $otro=25;
         return view ('pagGaleria' ,compact('valor', 'otro'));
     }
+    public function fnRegistrar(Request $request){
+
+        //return $request->all();         //Prueba de "token" y datos recibidos
+
+        $request ->validate([
+            'codEst' => 'required',
+            'nomEst' => 'required',
+            'apeEst' => 'required',
+            'fnaEst' => 'required',
+            'turnMat' => 'required',
+            'semMat' => 'required',
+            'estMat' => 'required'
+        ]);
+
+        $nuevoEstudiante = new Estudiante1;
+        $nuevoEstudiante->codEst = $request->codEst;
+        $nuevoEstudiante->nomEst = $request->nomEst;
+        $nuevoEstudiante->apeEst = $request->apeEst;
+        $nuevoEstudiante->fnaEst = $request->fnaEst;
+        $nuevoEstudiante->turnMat = $request->turnMat;
+        $nuevoEstudiante->semMat = $request->semMat;
+        $nuevoEstudiante->estMat = $request->estMat;
+        
+        $nuevoEstudiante->save();
+        
+        //$xAlumnos = Estudiante1::all();                      //Datos de BD
+        //return view('pagLista', compact('xAlumnos'));        //Pasar a pagLista
+        return back()->with('msj','Se registro con éxito...'); //Regresar con msj
+    }
+    public function fnEstActualizar($id){                   //Paso 1
+
+        $xActAlumnos = Estudiante1::findOrFail($id);
+        return view('Estudiante.pagActualizar', compact('xActAlumnos'));
+    }
+
+    public function fnUpdate(Request $request, $id){        //Paso 2
+
+        //return $request->all();         //Prueba de "token" y datos recibidos
+
+        $xUpdateAlumnos = Estudiante1::findOrFail($id);
+
+        $xUpdateAlumnos->codEst = $request->codEst;
+        $xUpdateAlumnos->nomEst = $request->nomEst;
+        $xUpdateAlumnos->apeEst = $request->apeEst;
+        $xUpdateAlumnos->fnaEst = $request->fnaEst;
+        $xUpdateAlumnos->turnMat = $request->turnMat;
+        $xUpdateAlumnos->semMat = $request->semMat;
+        $xUpdateAlumnos->estMat = $request->estMat;
+        
+        $xUpdateAlumnos->save();
+        
+        //$xAlumnos = Estudiante1::all();                        //Datos de BD
+        //return view('pagLista', compact('xAlumnos'));          //Pasar a pagLista
+        return back()->with('msj','Se actualizó con éxito...');  //Regresar con msj
+    }
+
+    //////////////////// DELETE /////////////////////////////////// 
+    public function fnEliminar(Request $request, $id){
+        $deleteAlumno = Estudiante1::findOrFail($id);
+        $deleteAlumno->delete();
+        return back()->with('msj','Se elimino con éxito');
+    
+    }
 }
